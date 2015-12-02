d30470d449b6a82415d396c492179a1f30a9cd36
Rails Udemy Course <railsudemycourse@gmail.com>
2015-12-02 21:30:07 +0000

    Add partials and destroy action

--- code/rails_projects/alpha-blog/app/controllers/articles_controller.rb
+++ code/rails_projects/alpha-blog/app/controllers/articles_controller.rb
@@ -46,6 +46,18 @@ class ArticlesController < ApplicationController
 
   end
 
+
+  def destroy
+
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    flash[:notice] = "Article Successllfully deteleted"
+
+    redirect_to articles_path
+  end
+
+
   private
   def article_params
     params.require(:article).permit(:title, :description)
