d139f56c2b8d8c9f1581e9701af1f532d265d228
Rails Udemy Course <railsudemycourse@gmail.com>
2015-12-02 20:55:57 +0000

    Add edit,update, show and index listing

--- code/rails_projects/alpha-blog/app/controllers/articles_controller.rb
+++ code/rails_projects/alpha-blog/app/controllers/articles_controller.rb
@@ -4,8 +4,14 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   def index
-
+    @articles = Article.all
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
 
   def create
     # render plain: params[:article].inspect
@@ -22,6 +28,17 @@ class ArticlesController < ApplicationController
 
       render 'new'
     end
+  end
+
+   def update
+    @article = Article.find (params[:id])
+    if @article.update (article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+
   end
 
   def show
--- code/rails_projects/alpha-blog/config/routes.rb
+++ code/rails_projects/alpha-blog/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  
 
   # resources :songs
   # The priority is based upon order of creation: first created -> highest priority.
