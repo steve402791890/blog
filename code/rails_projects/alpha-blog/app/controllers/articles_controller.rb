4258499adc4bae2a4884c14fa29212690a7fb133
Rails Udemy Course <railsudemycourse@gmail.com>
2015-12-02 18:07:12 +0000

    fix create error message, create show

--- code/rails_projects/alpha-blog/app/controllers/articles_controller.rb
+++ code/rails_projects/alpha-blog/app/controllers/articles_controller.rb
@@ -3,18 +3,32 @@ class ArticlesController < ApplicationController
 
     @article = Article.new
   end
+  def index
+
+  end
 
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    # @article.save
+    # redirect_to articles_path(@article)
+    if @article.save
+      #do something
+
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+
+
+      render 'new'
+    end
   end
 
   def show
-    
+    @article = Article.find(params[:id])
+
   end
-  
+
   private
   def article_params
     params.require(:article).permit(:title, :description)
--- code/rails_projects/alpha-blog/app/models/article.rb
+++ code/rails_projects/alpha-blog/app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  # validates :title, presence: ture, length: { minimum: 3, maximum: 50 }
-  # validates :description, presence: ture, length: { minimum: 10, maximum: 300 }
-
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  # validates :name, presence: true
 end
